dc7f7576c3b31d329840d181e0790683a94fc048
TakahiroKz <KizugaTakahiro@gmail.com>
2023-05-15 16:54:17 -0500

    Se termina la aplicacion web, añadiendo css, html, urlpatterns y cosas demas

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='blog/images')
+    date = models.DateField(timezone.now())
+
+    def __str__(self):
+        return self.title
+
